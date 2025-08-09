5fb7a96637671866f317a9f4f54e2223b228de1b
Jiarui-Shuai <sjr_3@outlook.com>
2025-08-09 13:52:21 +0800

    commit

--- src/App.java
+++ src/App.java
@@ -6,8 +6,9 @@ public class App {
         System.out.println("Hello, World!");
         String input = inputs.input("Enter your name: ");
         System.out.println("Hello, "+input+"!");
+        System.out.println(module.times_str(3, "="));
         for(int i=0; i<10000; i++){
-            System.out.print("loading: |"+'='*(i/10000*10)+' '*(10-i/10000*10)+"|\r");
+            System.out.print("loading: |"+module.times_str(i/10000*10, "=")+module.times_str((10-i/10000*10)," ")+"|\r");
             module.sleep(0.001);
         }
         System.out.println("");
